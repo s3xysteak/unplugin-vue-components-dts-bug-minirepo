b7a15d632c4e72c1f4760813420179f7e3370a4c
s3xysteak <j.yulia@qq.com>
2023-11-30 16:20:31 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,24 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import Component from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Component({
+      dts: '/types/Components.d.ts',
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false
+        })
+        /**
+         * AntDesignVueResolver() will throw error
+         */
+      ]
+    })
   ],
   resolve: {
     alias: {
